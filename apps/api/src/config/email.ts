801aebe632bf0231d1b3892f2163a386a43aa160
Chaitra <chaitrahosur3112@gmail.com>
2026-09-05 22:51:28 +0530

    FIxed the port

--- apps/api/src/config/email.ts
+++ apps/api/src/config/email.ts
@@ -9,7 +9,7 @@ export async function getEmailTransporter(): Promise<Transporter> {
   if (process.env.NODE_ENV === "production") {
     transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT ?? 587),
+      port: Number(process.env.SMTP_PORT ?? 465),
       auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
     });
   } else {
